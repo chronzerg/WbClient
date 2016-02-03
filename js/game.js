0893cdc9d1cc08174dfc607b133fde497be1d4dc
Jon Anderson <jon.david.anderson@gmail.com>
2016-02-02 19:52:04 -0800

    deploy

---
+++ js/game.js
@@ -0,0 +1,110 @@
+define(['client2Server', 'logging'], function gameModule (Server, logging) {
+	'use strict';
+
+	const url = 'wss://wishbanana.herokuapp.com',
+	//const url = 'ws://localhost:5000',
+		  stateNames = ['Connecting', 'Matching', 'Counting', 'Playing', 'Ending'];
+
+	logging = logging('Server');
+	var log = logging.log;
+
+	return function Game (name) {
+		var thisGame = this,
+		    playing = true,
+		    state = 0,
+
+		    server = new Server(url);
+
+		var changeState = function (newState) {
+			if (newState - state === 1) {
+				state = newState;
+				log('Game state changed: ' + state);
+				return true;
+			}
+
+			log ('Invalid state change requested: ' + state + ' to ' + newState, logging.WARNING);
+			return false;
+		};
+
+		server.onClose = function () {
+			playing = false;
+		};
+
+		server.onConnected = function () {
+			thisGame.onConnected();
+		};
+
+		server.onWinCount = function (count) {
+			thisGame.onWinCount(count);
+		};
+
+		server.onNamePlease = function () {
+			if (changeState(1)) {
+				server.name(name);
+			}
+		};
+
+		server.onMatched = function (opponentName) {
+			if (changeState(2)) {
+				thisGame.onMatched(opponentName);
+			}
+		};
+
+		server.onCountDown = function (value) {
+			if (state !== 2) {
+				log('Received countdown message during state ' + state, logging.WARNING);
+			}
+
+			if (value > 0) {
+				thisGame.onCountDown(value);
+			}
+			else if (changeState(3)) {
+				thisGame.onPlaying();
+			}
+		};
+
+		server.onClickCount = function (yourClicks, theirClicks) {
+			thisGame.count = yourClicks;
+			thisGame.onClickCount(yourClicks, theirClicks);
+		};
+
+		server.onGameOver = function (youWon) {
+			if (changeState(4)) {
+				thisGame.onGameOver(youWon);
+				thisGame.quit();
+			}
+		};
+
+		this.click = function () {
+			if (state === 3) {
+				this.count++;
+				server.click();
+			}
+		};
+
+		this.quit = function () {
+			if (playing) {
+				server.close();
+				playing = false;
+
+				server.onClose = undefined;
+				server.onConnected = undefined;
+				server.onWinCount = undefined;
+				server.onNamePlease = undefined;
+				server.onMatched = undefined;
+				server.onCountDown = undefined;
+				server.onClickCount = undefined;
+				server.onGameOver = undefined;
+			}
+		};
+
+		this.onConnected = undefined;
+		this.onWinCount = undefined;
+		this.onMatched = undefined;
+		this.onCountDown = undefined;
+		this.onPlaying = undefined;
+		this.onClickCount = undefined;
+		this.onGameOver = undefined;
+		this.count = 0;
+	};
+});
---
+++ js/wishbanana.js
@@ -0,0 +1,168 @@
+requirejs.config({
+	baseurl: 'js',
+	shim: {
+		imagesloaded: ['jquery']
+	}
+});
+
+define(['jquery', 'imagesloaded', 'paging', 'animations', 'game', 'logging'], function wishbananaModule ($, imagesloaded, paging, animations, Game, logging) {
+	'use strict';
+
+	logging = logging('wishbanana');
+	var log = logging.log;
+
+	$(function onDocumentReady () {
+		// Check for chrome
+		if (navigator.userAgent.toLowerCase().indexOf('chrome') < 0) {
+			$('#loadingMessage').hide();
+			$('#errorMessage').show();
+
+			return;
+		}
+
+		var mainPaging = paging($('body > div.page'));
+
+		// When all images are loaded, switch to the menu page.
+		$('img').imagesLoaded().always(function imagesLoaded () {
+			mainPaging.switchToPage('menu');
+		});
+
+		// MENU ----------------------------------------------------------
+		$('button#menuToStory').click(function onMenuToStoryClick () {
+			mainPaging.switchToPage('story');
+		});
+
+		$('button#menuToGame').click(function onMenuToGameClick () {
+			mainPaging.switchToPage('game');
+		});
+
+		$('button#openHelp').click(function onOpenHelpClick (event) {
+			event.stopPropagation();
+			$('#helpModal, #menu > .tint').show();
+
+			$(document).one('click', function onHelpModalClick () {
+				$('#helpModal, #menu > .tint').hide();
+			});
+		});
+
+		// STORY ---------------------------------------------------------
+		$('button#storyToMenu').click(function onStoryToMenuClick () {
+			mainPaging.switchToPage('menu');
+		});
+
+		// GAME ----------------------------------------------------------
+		var WIN_CLICKS = 1;
+
+		var gamePaging = paging($('div#game').find('div.state')),
+		    countingPaging = paging($('div#counting').find('div.count')),
+		    g = null;
+
+		var updateYourClicks = function (yourClicks) {
+			animations.updateYourProgress(yourClicks / WIN_CLICKS);
+		};
+
+		var updateTheirClicks = function (theirClicks) {
+			animations.updateTheirProgress(theirClicks / WIN_CLICKS);
+		};
+
+		var playingMouseDown = function () {
+			g.click();
+			animations.flash();
+			updateYourClicks(g.count);
+		};
+
+		var initNewGame = function (name) {
+			g = new Game(name);
+			g.onConnected = function () {
+				$('#matching > h2').html('matching...');
+			};
+			g.onWinCount = function (count) {
+				WIN_CLICKS = count;
+				if (WIN_CLICKS < 1) {
+					WIN_CLICKS = 1;
+				}
+			};
+			g.onMatched = function (opponentName) {
+				$('.theirName').html(opponentName);
+				gamePaging.switchToPage('counting');
+			};
+			g.onCountDown = function (value) {
+				$('#count > h1').html(value);
+			};
+			g.onPlaying = function () {
+				gamePaging.switchToPage('playing', true);
+			};
+			g.onClickCount = function (yourClicks, theirClicks) {
+				updateYourClicks(yourClicks);
+				updateTheirClicks(theirClicks);
+			};
+			g.onGameOver = function (youWon) {
+				animations.gameOver(youWon, function gameOverComplete () {
+					if (youWon) {
+						$('#youWin').show();
+					}
+					else {
+						$('#youLose').show();
+					}
+					$('#gameOverBanner').fadeIn();
+				});
+				$(document).off('mousedown', playingMouseDown);
+			};
+		};
+
+		mainPaging.addBeforeShowCallback('game', function gameBeforeShow () {
+			gamePaging.switchToPage('naming');
+		});
+		mainPaging.attachChildPaging('game', gamePaging);
+
+		gamePaging.addAfterShowCallback('naming', function namingAfterShowCallback () {
+			$('input#name').focus();
+		});
+		gamePaging.addBeforeShowCallback('matching', function matchingBeforeShow () {
+			$('#matching > h2').html('connecting...');
+		});
+		gamePaging.addBeforeShowCallback('counting', function countingBeforeShow () {
+			$('#count > h1').html('');
+		});
+		gamePaging.addBeforeShowCallback('playing', function playingBeforeShow () {
+			$(document).on('mousedown', playingMouseDown);
+
+			$('#youWin').hide();
+			$('#youLose').hide();
+			$('#gameOverBanner').hide();
+
+			animations.reset();
+			animations.attachResizeHandler();
+		});
+		gamePaging.addBeforeHideCallback('playing', function playingBeforeHide () {
+			$(document).off('mousedown', playingMouseDown);
+			animations.detachResizeHandler();
+
+			if (g !== null) {
+				g.quit();
+				g = null;
+			}
+		});
+
+		$('button#gameToMenu').click(function onGameToMenuClick (event) {
+			mainPaging.switchToPage('menu');
+			event.stopPropagation();
+		});
+		$('input#name').keydown(function nameInputEnterKey (e) {
+			if (e.keyCode == 13) {
+				$('button#namingDone').click();
+			}
+		});
+		$('button#namingDone').click(function startNewGame () {
+			var name = $('input#name').val();
+			$('.yourName').html(name);
+
+			initNewGame(name);
+
+			gamePaging.switchToPage('matching');
+		});
+		$('#playAgain').click(function onPlayAgainClick () {
+			gamePaging.switchToPage('naming');
+		});
+	});
+});
